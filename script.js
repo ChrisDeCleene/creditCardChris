02738a6b8a95c5c6cd165133c4e5d611f89d2bb8
Chris DeCleene <chris_decleene@ymail.com>
2021-07-28 16:39:25 -0600

    Add onclick interactivity.

--- script.js
+++ script.js
@@ -15,3 +15,52 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Dark Theme Button
+function changeTheme () {
+    document.body.style.backgroundColor = 'black';
+    document.querySelector('main').className = 'easterEgg';
+    document.querySelector('header').className = 'easterEgg';
+    document.querySelector('h1').className = 'easterEgg';
+    let h2 = document.querySelectorAll('h2')
+    for (let i = 0; i < h2.length; i++) {
+        h2[i].className = 'easterEgg';
+    }
+    let button = document.querySelectorAll('button')
+    for (let i = 0; i < button.length; i++) {
+        button[i].className = 'easterEgg';
+    }
+    let contactMe = document.getElementById('contactme')
+    contactMe.className = 'easterEgg';
+    document.querySelector('footer').className = 'easterEgg';
+}
+
+document.getElementById('easterEgg').onclick = changeTheme;
